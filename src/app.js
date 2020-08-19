c5e8b981d13e53cc93f2df69b485e047ecb15b64
Michal Front <michal.front@outlook.com>
2020-08-19 18:25:52 +0200

    transformer works, doesn't deselect

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useRef } from "react";
-import { Stage, Layer, Text, Image } from "react-konva";
+import { Stage, Layer, Text, Image, Transformer } from "react-konva";
 import { useDispatch, useSelector } from "react-redux";
 import { getLager } from "./actions";
 
@@ -9,7 +9,11 @@ export default function Canvas() {
     //i set it to an array so the user could choose multiple pieces of furniture
     const [selectedImage, setSelectedImage] = useState([]);
     const imageRef = useRef(null);
+    const scaleRef = useRef(null);
     const dispatch = useDispatch();
+    const trRef = useRef(null);
+    let updateImage;
+    let newImageState;
 
     useEffect(() => {
         dispatch(getLager());
@@ -17,24 +21,20 @@ export default function Canvas() {
     }, []);
 
     const lager = useSelector((state) => state.lager);
-    const text = new Konva.Text({
-        x: 10,
-        y: 10,
-        fontFamily: "Calibri",
-        fontSize: 24,
-        text: "",
-        fill: "black",
-    });
 
-    function writeMessage(message) {
-        text.text(message);
-    }
+    const checkDeselect = (e) => {
+        // deselect when clicked on empty area
+        const clickedOnEmpty = e.target === e.target.getStage();
+        if (clickedOnEmpty) {
+            toggleTransformer(null);
+        }
+    };
 
-    function loadImage(src) {
+    function loadImage(src, name, id) {
         const img = new window.Image();
-        //img.x = 0;
-        //img.y = 0;
         img.src = src;
+        img.name = name;
+        img.id = id;
         img.crossOrigin = "Anonymous";
         imageRef.current = img;
         imageRef.current.addEventListener("load", handleLoad);
@@ -46,30 +46,26 @@ export default function Canvas() {
             ...selectedImage,
             { image: imageRef.current, x: 0, y: 0 },
         ]);
-        //console.log("imageRef: ", imageRef);
     }
 
     function getImage(e) {
-        console.log("this image was clicked:", e.target.name, e.target.id);
-        loadImage(e.target.src);
+        //console.log("this image was clicked:", e.target.name, e.target.id);
+        loadImage(e.target.src, e.target.name, e.target.id);
     }
 
     function updatePosition(i, e) {
-        let lastPos = e.target._lastPos;
-        console.log("i: ", i);
+        let pos = e.target.attrs;
+        let currX = pos.x;
+        let currY = pos.y;
+        updateImage = selectedImage[i];
+        //console.log("updateImagePos: ", updateImagePos);
+        updateImage.x = currX;
+        updateImage.y = currY;
+        //console.log("updateImagePos after,...: ", updateImagePos);
 
-        let currX = lastPos.x;
-        let currY = lastPos.y;
-
-        let updateImagePos = selectedImage[i];
-        console.log("updateImagePos: ", updateImagePos);
-        updateImagePos.x = currX;
-        updateImagePos.y = currY;
-        console.log("updateImagePos after,...: ", updateImagePos);
-
-        let newImageState = selectedImage.map((img, idx) => {
+        newImageState = selectedImage.map((img, idx) => {
             if (idx == i) {
-                return updateImagePos;
+                return updateImage;
             } else {
                 return img;
             }
@@ -77,14 +73,40 @@ export default function Canvas() {
 
         setSelectedImage(newImageState);
         //console.log("imageRef in updatePOSITION: ", imageRef);
-        console.log("newImageState after mapping: ", newImageState);
+        //console.log("newImageState after mapping: ", newImageState);
         //console.log("selectedImage: ", selectedImage);
-
-        console.log("currX, currY: ", currX, currY);
     }
     console.log("selectedImage: ", selectedImage);
 
-    //console.log("image: ", image);
+    function toggleTransformer(i, e) {
+        console.log("scaleRef in toggleTransformer: ", scaleRef.current);
+        //console.log("i: ", i);
+        trRef.current.node(scaleRef.current);
+        trRef.current.getLayer().batchDraw();
+    }
+
+    function updateSize(i, e) {
+        const node = e.target.attrs;
+
+        updateImage = selectedImage[i];
+
+        updateImage.x = node.x;
+        updateImage.y = node.y;
+        node.image.height = node.image.height * node.scaleY;
+        node.image.width = node.image.width * node.scaleX;
+        //console.log("updateImagePos after,...: ", updateImagePos);
+
+        newImageState = selectedImage.map((img, idx) => {
+            if (idx == i) {
+                return updateImage;
+            } else {
+                return img;
+            }
+        });
+
+        setSelectedImage(newImageState);
+    }
+    console.log("updateImage: ", updateImage);
 
     return (
         <div id="test">
@@ -105,7 +127,12 @@ export default function Canvas() {
                     })}
             </div>
 
-            <Stage className="canvas" width={innerWidth} height={innerHeight}>
+            <Stage
+                className="canvas"
+                width={innerWidth}
+                height={innerHeight}
+                onMouseDown={checkDeselect}
+            >
                 <Layer>
                     <Text
                         text="Click on an image to load it on the canvas!"
@@ -120,11 +147,23 @@ export default function Canvas() {
                                     x={img.x}
                                     y={img.y}
                                     image={img.image}
+                                    name={img.name}
+                                    id={img.id}
                                     draggable
                                     onDragEnd={(e) => {
                                         updatePosition(i, e);
                                     }}
+                                    onClick={(e) => {
+                                        toggleTransformer(i, e);
+                                    }}
+                                    ref={scaleRef}
+                                    onTransformEnd={(e) => {
+                                        updateSize(i, e);
+                                    }}
                                 />
+                                {toggleTransformer && (
+                                    <Transformer ref={trRef} />
+                                )}
                             </Layer>
                         );
                     })}
